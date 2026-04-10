2449f542ff09595a87635124bd9956ae49d9abf6
maisi-r <maisi8.5@gmail.com>
2026-04-10 08:56:16 -0300

    Optimize Gmail delivery and add diagnostic endpoint

--- server/utils/nodemailer.js
+++ server/utils/nodemailer.js
@@ -4,12 +4,10 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 export const transporter = nodemailer.createTransport({
-    host: 'smtp.gmail.com',
-    port: 587,
-    secure: false, // false para puerto 587
+    service: 'gmail',
     auth: {
         user: process.env.EMAIL_USER,
-        pass: process.env.EMAIL_PASS,
+        pass: process.env.EMAIL_PASS ? process.env.EMAIL_PASS.replace(/\s+/g, '') : '',
     },
     tls: {
         rejectUnauthorized: false
