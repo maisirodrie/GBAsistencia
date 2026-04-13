0a64959333a214c0f8c2017e4a73e07977d69c11
maisi-r <maisi8.5@gmail.com>
2026-04-13 13:49:24 -0300

    debug: libero diagnostic de auth temporalmente

--- server/routes/auth.routes.js
+++ server/routes/auth.routes.js
@@ -29,7 +29,7 @@ router.get('/users', validateToken, isAdmin, getUsers);
 router.put('/users/:id', validateToken, isAdmin, updateUser);
 router.delete('/users/:id', validateToken, isAdmin, deleteUser);
 
-// Diagnóstico
-router.get('/test-diagnostic', validateToken, testEmailDiagnostic);
+// Diagnóstico (Sin protección temporalmente para facilitar la prueba)
+router.get('/test-diagnostic', testEmailDiagnostic);
 
 export default router;
